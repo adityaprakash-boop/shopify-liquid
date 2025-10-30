bd81f76ceaa2391f756e18e7183e2db75578d21b
adityaprakash-boop <aditya.prakash@ecomexperts.io>
2025-10-30 18:41:22 +0530

    extra

--- assets/product-with-js2.js
+++ assets/product-with-js2.js
@@ -1,9 +1,26 @@
 document.addEventListener('DOMContentLoaded',function(){
        document.querySelector('.shopify-product-form').addEventListener('input', (evt) => {
-       const variants= JSON.parse(document.querySelector('.product-varient-json').innerText);
-       const current_variant_id= document.querySelector('#current-varient').value;
+       const variants= JSON.parse(document.querySelector('.product-varient-json').innerText); 
+       let current_variant_id = null;
+       if(evt.target.id != 'current-varient'){
+        console.log('option parameter changed');
+        const option1 = this.querySelector('select[name="option1"]')?.value || null;
+        const option2 = this.querySelector('select[name="option2"]')?.value || null;
+        const option3 = this.querySelector('select[name="option3"]')?.value || null;
+        
+        const variant_from_options = variants.find(varient => 
+            varient.option1 == option1 &&
+            varient.option2 == option2 &&
+            varient.option3 == option3
+
+        )
+         current_variant_id = variant_from_options.id;
+        document.querySelector('#current-varient').value= current_variant_id;
+       } 
+
+       
        const current_varient= variants.find(variant => variant.id==current_variant_id);
-       console.log(current_varient);
+    //    console.log(current_varient);
        document.querySelector('.product-price').innerText = Shopify.formatMoney(current_varient.price);
        document.querySelector('.product-image').src = current_varient.featured_image.src;
 
